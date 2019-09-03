58218c49d16a947fbc2f0f882346f85da318abfc
shentong.zhou <shentong.zhou@dianping.com>
2019-09-03 23:43:49 +0800

    add executor set mapper info feature

--- mybatis-generator-extention-core/src/main/java/io/github/elricboa/GenerateCodeExecutor.java
+++ mybatis-generator-extention-core/src/main/java/io/github/elricboa/GenerateCodeExecutor.java
@@ -2,7 +2,8 @@ package io.github.elricboa;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
-import org.mybatis.generator.config.Configuration;
+import org.apache.commons.lang.StringUtils;
+import org.mybatis.generator.config.*;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.exception.XMLParserException;
 
@@ -16,6 +17,18 @@ import java.util.List;
  */
 public class GenerateCodeExecutor {
 
+    private static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+
+    private String databaseUrl;
+    private String databaseUsername;
+    private String databasePassword;
+
+    private String javaCodeBasePath = System.getProperty("user.dir") + "/";
+    private String resourcesBasePath = System.getProperty("user.dir") + "/";
+    private String domainPackage;
+    private String mapperPackage;
+    private String mapperXmlFolder;
+
     private List<String> warnings = Lists.newArrayList();
 
     public void execute() throws IOException, XMLParserException {
@@ -24,5 +37,49 @@ public class GenerateCodeExecutor {
         //  解析器
         ConfigurationParser parser = new ConfigurationParser(warnings);
         Configuration configuration = parser.parseConfiguration(new File(generatorConfig.getFile()));
+        for (Context context : configuration.getContexts()) {
+            JDBCConnectionConfiguration jdbcConfig = context.getJdbcConnectionConfiguration();
+            jdbcConfig.setDriverClass(MYSQL_DRIVER);
+            if (StringUtils.isNotEmpty(databaseUrl)) {
+                jdbcConfig.setConnectionURL(databaseUrl);
+            }
+            if (StringUtils.isNotEmpty(databaseUsername)) {
+                jdbcConfig.setUserId(databaseUsername);
+            }
+            if (StringUtils.isNotEmpty(databasePassword)) {
+                jdbcConfig.setPassword(databasePassword);
+            }
+        }
+
+        for (Context context : configuration.getContexts()) {
+            JavaModelGeneratorConfiguration javaModelConfig = context.getJavaModelGeneratorConfiguration();
+            if (StringUtils.isNotEmpty(javaCodeBasePath)) {
+                javaModelConfig.setTargetProject(javaCodeBasePath + javaModelConfig.getTargetProject());
+            }
+            if (StringUtils.isNotEmpty(domainPackage)) {
+                javaModelConfig.setTargetPackage(domainPackage);
+            }
+        }
+
+        for (Context context : configuration.getContexts()) {
+            SqlMapGeneratorConfiguration mapperXmlConfig = context.getSqlMapGeneratorConfiguration();
+            if (StringUtils.isNotEmpty(resourcesBasePath)) {
+                mapperXmlConfig.setTargetProject(resourcesBasePath + mapperXmlConfig.getTargetProject());
+            }
+            if (StringUtils.isNotEmpty(mapperXmlFolder)) {
+                mapperXmlConfig.setTargetPackage(mapperXmlFolder);
+            }
+        }
+
+        for (Context context : configuration.getContexts()) {
+            JavaClientGeneratorConfiguration mapperConfig = context.getJavaClientGeneratorConfiguration();
+            if (StringUtils.isNotEmpty(javaCodeBasePath)) {
+                mapperConfig.setTargetProject(javaCodeBasePath + mapperConfig.getTargetProject());
+            }
+            if (StringUtils.isNotEmpty(mapperPackage)) {
+                mapperConfig.setTargetPackage(mapperPackage);
+            }
+        }
+
     }
 }
